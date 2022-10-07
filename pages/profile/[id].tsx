06deed42cf6ce558fc2838548779268501e44f5f
Sebastian <sebastianwuczkowski@gmail.com>
2022-10-07 13:42:12 +0200

    folder structre

---
+++ pages/profile/[id].tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const ProfileId = () => {
+  return <div>ProfileId</div>;
+};
+
+export default ProfileId;
--- pages/profile/index.tsx
+++ pages/profile/index.tsx
@@ -1,7 +1,19 @@
 import React from "react";
 
 const ProfilePage = () => {
-  return <div>ProfilePage</div>;
+  return <div className="wrapper">
+    <div className="mainInfo">
+<div className="left"></div>
+<div className="right"></div>
+
+
+    </div>
+    <div className="skills"></div>
+    <div className="experience"></div>
+    <div className="projects">
+
+    </div>
+  </div>
 };
 
 export default ProfilePage;
