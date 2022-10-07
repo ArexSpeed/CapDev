5d8de4ee42651c05a575d5e854b846e50e279d7c
Sebastian <sebastianwuczkowski@gmail.com>
2022-10-07 15:19:58 +0200

    folder updated

--- pages/profile/index.tsx
+++ pages/profile/index.tsx
@@ -1,16 +1,48 @@
 import React from "react";
 
 const ProfilePage = () => {
-  return <div className="wrapper">
-    <div className="mainInfo">
-<div className="left"></div>
-<div className="right"></div>
+  return <div className="flex flex-col">
+    <div className="flex flex-row w-full" >
+
+  <div >
+    <div className="">
+      Picture
+    </div>
+    <div className="">
+
+      socials
+    </div>
+    </div>
+    <div className="description">
+  <h2>Developer Name</h2>
+  <h3>Frontend Developer</h3>
+  XXX
+    
+  
+</div>
+<div className="flex-row">
+  <div className="flex justify-between ">
+    <h4>About me</h4>
+    <div>X</div>
+  </div>
+  <span>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Optio, voluptate necessitatibus saepe amet nobis reiciendis.</span>
+</div>
 
 
     </div>
-    <div className="skills"></div>
-    <div className="experience"></div>
+    <div className="skills">
+      <h4>Skills</h4>
+    </div>
+    <div className="experienceEducation flex flex-row">
+    <div className="experience">
+      <h4>Experience</h4>
+    </div>
+    <div className="education">
+      <h4>Education</h4>
+    </div>
+    </div>
     <div className="projects">
+      <h4>Projects</h4>
 
     </div>
   </div>
