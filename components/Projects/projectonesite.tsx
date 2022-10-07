2d7f052cbde0b1e1c72d74b3679246707e1dd3a8
Staxar <marcin.kepno@gmail.com>
2022-10-07 16:10:02 +0200

    Initial ProjectOneSite

--- components/Projects/projectonesite.tsx
+++ components/Projects/projectonesite.tsx
@@ -1,7 +1,7 @@
 export default function ProjectOneSite() {
   return (
     <div className="">
-      <div className="flex fle-row"></div>
+      <div className="flex flex-row"></div>
     </div>
   );
 }
