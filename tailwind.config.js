a0e18c4e1d23111c0a2bb400a711983df84f3df2
Arek <arkadiusz.cichocki@capgemini.com>
2022-10-07 15:11:08 +0200

    add custom tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,27 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
+    colors: {
+      'primary': '#FFFFFF',
+      'secondary': '#F3F3F3',
+      'primary-dark': '#111827',
+      'secondary-dark': '#000000',
+      'gray': '#555555',
+      'blue': '#0072AF',
+      'lightblue': '#17ABDB',
+      'green': '#95E616',
+    },
+    fontSize: {
+      12: '12px',
+      16: '16px',
+      20: '20px', 
+      24: '24px', 
+      32: '32px', 
+    },
+    fontFamily: {
+      roboto: ['Roboto', 'sans-serif'],
+      serif: ['Merriweather', 'serif'],
+    },
     extend: {},
   },
   plugins: [],
