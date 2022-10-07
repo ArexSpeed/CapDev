e36518e8e40e2392d4e6b92121507ae294cc1417
Dmytro <dmytro.zaichenko@capgemini.com>
2022-10-07 15:53:41 +0200

    adjust styles

--- components/Layout/index.tsx
+++ components/Layout/index.tsx
@@ -11,7 +11,7 @@ export const Layout: FC<Props> = ({ children }) => {
   return (
     <div>
       <Header />
-      <div className="flex  w-full h-full">
+      <div className="flex w-full h-full">
         <Navigation />
         <main className="flex bg-slate-300 text-black ml-[256px] w-full h-full ">{children}</main>
       </div>
