84239054dd6a863448ddb79b745178a08f3950dc
Sebastian <sebastianwuczkowski@gmail.com>
2022-10-08 14:34:01 +0200

    small changes

--- pages/profile/index.tsx
+++ pages/profile/index.tsx
@@ -1,4 +1,6 @@
 import Education from "components/Education";
+import { BritainIcon, GermanyIcon, PolandIcon } from "components/Icons/FlagIcons";
+import { FacebookIcon, LinkedinIcon, TwitterIcon } from "components/Icons/SocialIcons";
 import { Layout } from "components/Layout";
 import ProjectCard from "components/Projects/projectcard";
 import Skill from "components/Skills/Skill";
@@ -6,30 +8,37 @@ import React from "react";
 
 const ProfilePage = () => {
   return <Layout> <div className="flex flex-col p-8">
-    <div className="flex flex-row w-full bg-white rounded-md p-4" >
-  <div className="p-2 flex w-7/12" >
-    <div>
-    <div className="flex flex-column">
-      Picture
-    </div>
-    <div className="">
+  <div className="flex flex-row w-full bg-white rounded-md p-4" >
+<div className="p-2 flex w-7/12" >
+  <div>
+  <div className="h-[150px] w-[150px] rounded-full bg-center bg-cover bg-cyan-600 mr-2 bg-no-repeat bg-[url('/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Fmy-photo.eb484903.jpg&w=96&q=75')]">
 
-      socials
     </div>
+  <div className="flex justify-center mt-8 ">
+    <div className="mx-1">
+    <FacebookIcon className="text-lightblue hover:text-blue cursor-pointer duration-300 w-6 h-6"/>
     </div>
-    <div className="description">
-  <h2>Developer Name</h2>
-  <h3>Frontend Developer</h3>
-  XXX
-    
-  
+    <div className="mx-1"><TwitterIcon className="text-lightblue hover:text-blue cursor-pointer duration-300 w-6 h-6"/></div>
+    <div className="mx-1"><LinkedinIcon className="text-lightblue hover:text-blue cursor-pointer duration-300 w-6 h-6"/></div>
+  </div>
+  </div>
+  <div className="ml-10">
+<h2 className="text-2xl font-bold my-2">Developer Name</h2>
+<h3 className="text-xl  my-2">Frontend Developer</h3>
+<div className="flex">
+<PolandIcon className = 'w-6 h-6 m-1'/>
+<GermanyIcon className = 'w-6 h-6 m-1'/>
+<BritainIcon className = 'w-6 h-6 m-1'/>
 </div>
-    </div>
-    
-<div className="flex-row w-5/12">
-  <div className="flex justify-between ">
-    <h4 className="my-3">About me</h4>
-    <div>X</div>
+</div>
+  </div>
+  
+<div className="flex flex-col w-5/12 px-10">
+<div className="flex justify-between">
+  <h4 className="my-3 ">About me</h4>
+    <div>
+
+      </div>
   </div>
   <span>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Optio, voluptate necessitatibus saepe amet nobis reiciendis.</span>
 </div>
