60bd20dd46775a5cec98db9622de1d67e07f7fa3
Sebastian <sebastianwuczkowski@gmail.com>
2022-10-08 12:57:24 +0200

    profille id

--- pages/profile/[id].tsx
+++ pages/profile/[id].tsx
@@ -1,7 +1,85 @@
+import Education from "components/Education";
+import { Layout } from "components/Layout";
+import ProjectCard from "components/Projects/projectcard";
+import Skill from "components/Skills/Skill";
 import React from "react";
 
 const ProfileId = () => {
-  return <div>ProfileId</div>;
+  return <Layout> <div className="flex flex-col p-8">
+  <div className="flex flex-row w-full bg-white rounded-md p-4" >
+<div className="p-2 flex w-7/12" >
+  <div>
+  <div className="flex flex-column">
+    Picture
+  </div>
+  <div className="">
+
+    socials
+  </div>
+  </div>
+  <div className="description">
+<h2>Developer Name</h2>
+<h3>Frontend Developer</h3>
+XXX
+  
+
+</div>
+  </div>
+  
+<div className="flex-row w-5/12">
+<div className="flex flex-col justify-between">
+  <h4 className="my-3">About me</h4>
+  <span>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Optio, voluptate necessitatibus saepe amet nobis reiciendis.</span>
+</div>
+<div className="w-full flex justify-end">
+<button className="h-[24px] w-[104px] rounded-md bg-[#17ABDB] text-white my-5 ">
+    <span className="self-center text-gray-900">Follow</span>
+  </button>
+</div>
+
+</div>
+
+
+  </div>
+  <div className="skills">
+    <h4 className="my-3">Skills</h4>
+    <div className="flex flex-wrap">
+    <Skill/>
+    <Skill/>
+    <Skill/>
+
+    </div>
+    
+  </div>
+  <div className="experienceEducation flex flex-row w-full">
+  <div className="experience w-1/2">
+    <h4 className="my-3">Experience</h4>
+    <Education/>
+    <Education/>
+    <Education/>
+
+    
+    
+  </div>
+  <div className="w-1/2 ">
+    <h4 className="my-3">Education</h4>
+    <Education/>
+    <Education/>
+    <Education/>
+  </div>
+  </div>
+  <div>
+    <h4 className="my-3">Projects</h4>
+    <div className=" flex gap-x-10 gap-y-10 flex-wrap">
+
+    <ProjectCard/>
+    <ProjectCard/>
+    <ProjectCard/>
+    <ProjectCard/>
+    </div>
+  </div>
+</div>
+</Layout>;
 };
 
 export default ProfileId;
--- pages/profile/index.tsx
+++ pages/profile/index.tsx
@@ -1,11 +1,15 @@
+import Education from "components/Education";
+import { Layout } from "components/Layout";
 import ProjectCard from "components/Projects/projectcard";
+import Skill from "components/Skills/Skill";
 import React from "react";
 
 const ProfilePage = () => {
-  return <div className="flex flex-col">
-    <div className="flex flex-row w-full" >
-  <div >
-    <div className="">
+  return <Layout> <div className="flex flex-col p-8">
+    <div className="flex flex-row w-full bg-white rounded-md p-4" >
+  <div className="p-2 flex w-7/12" >
+    <div>
+    <div className="flex flex-column">
       Picture
     </div>
     <div className="">
@@ -20,9 +24,11 @@ const ProfilePage = () => {
     
   
 </div>
-<div className="flex-row">
+    </div>
+    
+<div className="flex-row w-5/12">
   <div className="flex justify-between ">
-    <h4>About me</h4>
+    <h4 className="my-3">About me</h4>
     <div>X</div>
   </div>
   <span>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Optio, voluptate necessitatibus saepe amet nobis reiciendis.</span>
@@ -31,20 +37,35 @@ const ProfilePage = () => {
 
     </div>
     <div className="skills">
-      <h4>Skills</h4>
-    </div>
-    <div className="experienceEducation flex flex-row">
-    <div className="experience">
-      <h4>Experience</h4>
+      <h4 className="my-3">Skills</h4>
+      <div className="flex flex-wrap">
+      <Skill/>
+      <Skill/>
+      <Skill/>
+
+      </div>
       
     </div>
-    <div className="education">
-      <h4>Education</h4>
+    <div className="experienceEducation flex flex-row w-full">
+    <div className="experience w-1/2">
+      <h4 className="my-3">Experience</h4>
+      <Education/>
+      <Education/>
+      <Education/>
+  
+      
+      
+    </div>
+    <div className="w-1/2 ">
+      <h4 className="my-3">Education</h4>
+      <Education/>
+      <Education/>
+      <Education/>
     </div>
     </div>
-    <div className="gap-x-5 gap-y-5">
-      <h4 className="gap-x-5 gap-y-5">Projects</h4>
-      <div className=" flex gap-x-5 gap-y-5">
+    <div>
+      <h4 className="my-3">Projects</h4>
+      <div className=" flex gap-x-10 gap-y-10 flex-wrap">
 
       <ProjectCard/>
       <ProjectCard/>
@@ -53,6 +74,7 @@ const ProfilePage = () => {
       </div>
     </div>
   </div>
+  </Layout>
 };
 
 export default ProfilePage;
