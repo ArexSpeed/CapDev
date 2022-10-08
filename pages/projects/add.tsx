21f2be439e69f293056b6d5bc87b95b98009a8c0
Arek <arkadiusz.cichocki@capgemini.com>
2022-10-08 18:19:46 +0200

    upload func, prepare projetc form

---
+++ pages/projects/add.tsx
@@ -0,0 +1,13 @@
+import ProjectForm from 'components/Forms/ProjectForm';
+import { Layout } from 'components/Layout';
+import React from 'react';
+
+const AddprojectPage = () => {
+  return (
+    <Layout>
+      <ProjectForm />
+    </Layout>
+  );
+};
+
+export default AddprojectPage;
